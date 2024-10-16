11afe336af42e22e15907f82478dcecdc5922c12
lichangxun <lichangxun234@126.com>
2024-10-17 00:18:18 +0800

    10.16

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,49 +16,66 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// ~I AM NOT DONE
 
-pub struct ReportCard {
-    pub grade: f32,
-    pub student_name: String,
-    pub student_age: u8,
-}
-
-impl ReportCard {
-    pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn generate_numeric_report_card() {
-        let report_card = ReportCard {
-            grade: 2.1,
-            student_name: "Tom Wriggle".to_string(),
-            student_age: 12,
-        };
-        assert_eq!(
-            report_card.print(),
-            "Tom Wriggle (12) - achieved a grade of 2.1"
-        );
-    }
-
-    #[test]
-    fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
-        let report_card = ReportCard {
-            grade: 2.1,
-            student_name: "Gary Plotter".to_string(),
-            student_age: 11,
-        };
-        assert_eq!(
-            report_card.print(),
-            "Gary Plotter (11) - achieved a grade of A+"
-        );
-    }
+enum Grade {  
+    Numeric(f32),  
+    Alphabetic(String),  
+}  
+  
+trait DisplayableGrade {  
+    fn to_string(&self) -> String;  
+}  
+  
+impl DisplayableGrade for Grade {  
+    fn to_string(&self) -> String {  
+        match self {  
+            Grade::Numeric(num) => num.to_string(),  
+            Grade::Alphabetic(alpha) => alpha.clone(),  
+        }  
+    }  
+}  
+  
+pub struct ReportCard {  
+    pub grade: Grade,  
+    pub student_name: String,  
+    pub student_age: u8,  
+}  
+  
+impl ReportCard {  
+    pub fn print(&self) -> String {  
+        format!("{} ({}) - achieved a grade of {}",  
+                &self.student_name, &self.student_age, self.grade.to_string())  
+    }  
+}  
+  
+#[cfg(test)]  
+mod tests {  
+    use super::*;  
+  
+    #[test]  
+    fn generate_numeric_report_card() {  
+        let report_card = ReportCard {  
+            grade: Grade::Numeric(2.1),  
+            student_name: "Tom Wriggle".to_string(),  
+            student_age: 12,  
+        };  
+        assert_eq!(  
+            report_card.print(),  
+            "Tom Wriggle (12) - achieved a grade of 2.1"  
+        );  
+    }  
+  
+    #[test]  
+    fn generate_alphabetic_report_card() {  
+        let report_card = ReportCard {  
+            grade: Grade::Alphabetic("A+".to_string()),  
+            student_name: "Gary Plotter".to_string(),  
+            student_age: 11,  
+        };  
+        assert_eq!(  
+            report_card.print(),  
+            "Gary Plotter (11) - achieved a grade of A+"  
+        );  
+    }  
 }
